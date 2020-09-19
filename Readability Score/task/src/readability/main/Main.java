1cd9849e0c3c1a55504d17c272d50a31a9560fa5
kosten00 <52483690+kosten00@users.noreply.github.com>
2020-09-19 13:18:12 +0700

    Update Main.java

--- Readability Score/task/src/readability/main/Main.java
+++ Readability Score/task/src/readability/main/Main.java
@@ -9,7 +9,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        String path = "/home/konstantinvasilev/IdeaProjects/Readability Score/Readability Score/task/src/readability/main/text.txt";
         String text = new String(Files.readAllBytes(Paths.get(args[0])));
         System.out.println(text);
         ReadabilityAnalyzer ra = new ReadabilityAnalyzer(text);
